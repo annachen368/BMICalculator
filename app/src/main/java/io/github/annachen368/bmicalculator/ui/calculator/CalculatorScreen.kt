41f814ae86ab206c67e57f83374dc6fb2cdc5f0b
anna <annachen368@gmail.com>
2025-05-03 16:34:44 -0700

    refactor: enhance layout and keyboard handling in CalculatorScreen

--- app/src/main/java/io/github/annachen368/bmicalculator/ui/calculator/CalculatorScreen.kt
+++ app/src/main/java/io/github/annachen368/bmicalculator/ui/calculator/CalculatorScreen.kt
@@ -6,10 +6,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
@@ -19,6 +19,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -62,135 +63,143 @@ fun CalculatorScreen(
     uiState: CalculatorUiState,
     onEvent: (CalculatorEvent) -> Unit
 ) {
-    Column(Modifier
-        .padding(innerPadding)
-        .fillMaxSize()) {
-        Box(modifier = Modifier.align(alignment = Alignment.CenterHorizontally)) {
-            Text(
-                text = "Today is ${uiState.date}",
-                style = MaterialTheme.typography.titleMedium,
-                modifier = Modifier.padding(top = 12.dp)
-            )
-        }
-
-        Spacer(Modifier.height(24.dp))
-
-        val optionSelected =
-            listOf(uiState.isCmKgRadioButtonSelected, !uiState.isCmKgRadioButtonSelected)
-        val optionEvents = listOf(
-            { onEvent(CalculatorEvent.OnCmKgRadioButtonSelected) }, // Wrap in lambdas {} defers the call
-            { onEvent(CalculatorEvent.OnFtLbsRadioButtonSelected) }
-        )
-        val optionTexts = listOf(uiState.cmKgRadioButtonText, uiState.ftLbsRadioButtonText)
-
-        // Modifier.selectableGroup(): Helps TalkBack understand that the radio buttons are grouped
-        Row(Modifier.selectableGroup()) {
-            for (i in optionTexts.indices) {
-                Row(
-                    modifier = Modifier
-                        .selectable(
-                            selected = optionSelected[i],
-                            onClick = optionEvents[i]
-                        )
-                        .padding(horizontal = 16.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(2.dp)
-                ) {
-                    RadioButton(
-                        selected = optionSelected[i],
-                        onClick = null // null because we handle click at Row level
-                    )
-                    Text(text = optionTexts[i])
-                }
-            }
-        }
-
-        Spacer(Modifier.height(24.dp))
-        if (uiState.isCmKgRadioButtonSelected) {
-            LabeledTextFieldRow("Height") {
-                Row {
-                    TextField(
-                        value = uiState.cmValue,
-                        onValueChange = { onEvent(CalculatorEvent.OnCmValueChange(it)) },
-                        label = {
-                            Text(text = "cm")
-                        },
-                        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                }
-            }
-
-            LabeledTextFieldRow("Weight") {
-                Row {
-                    TextField(
-                        value = uiState.kgValue,
-                        onValueChange = { onEvent(CalculatorEvent.OnKgValueChange(it)) },
-                        label = {
-                            Text(text = "kg")
-                        },
-                        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                }
-            }
-        } else {
-            LabeledTextFieldRow("Height") {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp)
-                ) {
-                    TextField(
-                        value = uiState.ftValue,
-                        onValueChange = { onEvent(CalculatorEvent.OnFtValueChange(it)) },
-                        label = {
-                            Text(text = "ft")
-                        },
-                        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-                        modifier = Modifier
-                            .weight(1f)
-                            .fillMaxWidth(),
-                    )
-                    TextField(
-                        value = uiState.inValue,
-                        onValueChange = { onEvent(CalculatorEvent.OnInValueChange(it)) },
-                        label = {
-                            Text(text = "in")
-                        },
-                        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-                        modifier = Modifier
-                            .weight(1f)
-                            .fillMaxWidth()
-                    )
-                }
-            }
-
-            LabeledTextFieldRow("Weight") {
-                Row(modifier = Modifier.fillMaxWidth()) {
-                    TextField(
-                        value = uiState.lbsValue,
-                        onValueChange = { onEvent(CalculatorEvent.OnLbsValueChange(it)) },
-                        label = {
-                            Text(text = "lbs")
-                        },
-                        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-                        modifier = Modifier
-                            .weight(1f)
-                            .fillMaxWidth()
-                    )
-                }
-            }
-        }
-        // Push everything below this down
-        Spacer(modifier = Modifier.weight(1f))
+    Scaffold(bottomBar = {
         HorizontalDivider()
         Box(
             modifier = Modifier
-                .align(alignment = Alignment.CenterHorizontally)
+                .fillMaxWidth()
+                .imePadding() // Pushes the button above the keyboard (tells Compose to add bottom padding equal to the height of the keyboard)
                 .padding(12.dp)
         ) {
-            Button({ onEvent(CalculatorEvent.OnEnterClick) }) {
+            Button(
+                onClick = { onEvent(CalculatorEvent.OnEnterClick) },
+                modifier = Modifier.align(Alignment.Center)
+            ) {
                 Text(text = "Enter")
+            }
+        }
+    }) { contentPadding ->
+        Column(
+            Modifier
+                .padding(contentPadding)
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            Box(modifier = Modifier.align(alignment = Alignment.CenterHorizontally)) {
+                Text(
+                    text = "Today is ${uiState.date}",
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.padding(top = 12.dp)
+                )
+            }
+
+            Spacer(Modifier.height(24.dp))
+
+            val optionSelected =
+                listOf(uiState.isCmKgRadioButtonSelected, !uiState.isCmKgRadioButtonSelected)
+            val optionEvents = listOf(
+                { onEvent(CalculatorEvent.OnCmKgRadioButtonSelected) }, // Wrap in lambdas {} defers the call
+                { onEvent(CalculatorEvent.OnFtLbsRadioButtonSelected) }
+            )
+            val optionTexts = listOf(uiState.cmKgRadioButtonText, uiState.ftLbsRadioButtonText)
+
+            // Modifier.selectableGroup(): Helps TalkBack understand that the radio buttons are grouped
+            Row(Modifier.selectableGroup()) {
+                for (i in optionTexts.indices) {
+                    Row(
+                        modifier = Modifier
+                            .selectable(
+                                selected = optionSelected[i],
+                                onClick = optionEvents[i]
+                            )
+                            .padding(horizontal = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(2.dp)
+                    ) {
+                        RadioButton(
+                            selected = optionSelected[i],
+                            onClick = null // null because we handle click at Row level
+                        )
+                        Text(text = optionTexts[i])
+                    }
+                }
+            }
+
+            Spacer(Modifier.height(24.dp))
+            if (uiState.isCmKgRadioButtonSelected) {
+                LabeledTextFieldRow("Height") {
+                    Row {
+                        TextField(
+                            value = uiState.cmValue,
+                            onValueChange = { onEvent(CalculatorEvent.OnCmValueChange(it)) },
+                            label = {
+                                Text(text = "cm")
+                            },
+                            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                }
+
+                LabeledTextFieldRow("Weight") {
+                    Row {
+                        TextField(
+                            value = uiState.kgValue,
+                            onValueChange = { onEvent(CalculatorEvent.OnKgValueChange(it)) },
+                            label = {
+                                Text(text = "kg")
+                            },
+                            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                }
+            } else {
+                LabeledTextFieldRow("Height") {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        TextField(
+                            value = uiState.ftValue,
+                            onValueChange = { onEvent(CalculatorEvent.OnFtValueChange(it)) },
+                            label = {
+                                Text(text = "ft")
+                            },
+                            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                            modifier = Modifier
+                                .weight(1f)
+                                .fillMaxWidth(),
+                        )
+                        TextField(
+                            value = uiState.inValue,
+                            onValueChange = { onEvent(CalculatorEvent.OnInValueChange(it)) },
+                            label = {
+                                Text(text = "in")
+                            },
+                            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                            modifier = Modifier
+                                .weight(1f)
+                                .fillMaxWidth()
+                        )
+                    }
+                }
+
+                LabeledTextFieldRow("Weight") {
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        TextField(
+                            value = uiState.lbsValue,
+                            onValueChange = { onEvent(CalculatorEvent.OnLbsValueChange(it)) },
+                            label = {
+                                Text(text = "lbs")
+                            },
+                            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                            modifier = Modifier
+                                .weight(1f)
+                                .fillMaxWidth()
+                        )
+                    }
+                }
             }
         }
     }
