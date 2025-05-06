d0666f279c68e49f630eeaf744c52fada78c1976
anna <annachen368@gmail.com>
2025-05-05 19:51:18 -0700

    feat: implement navigation between Calculator and Result screens

--- app/src/main/java/io/github/annachen368/bmicalculator/MainActivity.kt
+++ app/src/main/java/io/github/annachen368/bmicalculator/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import io.github.annachen368.bmicalculator.ui.calculator.CalculatorEvent
 import io.github.annachen368.bmicalculator.ui.calculator.CalculatorScreen
 import io.github.annachen368.bmicalculator.ui.calculator.CalculatorViewModel
+import io.github.annachen368.bmicalculator.ui.navigation.Routes
+import io.github.annachen368.bmicalculator.ui.result.ResultScreen
 import io.github.annachen368.bmicalculator.ui.theme.BMICalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,13 +23,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BMICalculatorTheme {
-                    val viewmodel: CalculatorViewModel = viewModel()
-                    val uiState by viewmodel.uiState.collectAsState()
-                    val onEvent = viewmodel::onEvent
-                    CalculatorScreen(
-                        uiState = uiState,
-                        onEvent = onEvent
-                    )
+                val viewmodel: CalculatorViewModel = viewModel()
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = Routes.CALCULATOR) {
+                    composable(route = Routes.CALCULATOR) {
+                        val uiState by viewmodel.uiState.collectAsState()
+                        CalculatorScreen(
+                            uiState = uiState,
+                            onEvent = { event ->
+                                if (event == CalculatorEvent.OnEnterClick) {
+                                    navController.navigate(Routes.RESULT)
+                                } else {
+                                    viewmodel.onEvent(event)
+                                }
+                            }
+                        )
+                    }
+
+                    composable(route = Routes.RESULT) {
+                        ResultScreen(viewmodel.toResultUiState()) {
+                            navController.navigate(Routes.CALCULATOR)
+                        }
+                    }
+                }
             }
         }
     }
---
+++ app/src/main/java/io/github/annachen368/bmicalculator/ui/navigation/Routes.kt
@@ -0,0 +1,6 @@
+package io.github.annachen368.bmicalculator.ui.navigation
+
+object Routes {
+    const val CALCULATOR = "Calculator"
+    const val RESULT = "Result"
+}
